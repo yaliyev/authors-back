4c06e503b8909745e27695abbe03949e901b51b3
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2024-01-11 17:58:06 +0400

    express server config

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello');
+
+
+var express = require('express');
+const app = express();
+
+let bodyParser = require('body-parser');
+
+require('dotenv').config();
+app.use(bodyParser.json());
+
+let PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`App listening on ${PORT}`);
+})
